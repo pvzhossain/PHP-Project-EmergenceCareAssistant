048c77b725173f7baa2ce8add667a0a1c34c7524
Parvez Hossain <pvzhossain@gmail.com>
2021-11-25 22:37:03 +0600

    Update AddAmbulence.php

--- Emergency_Care_Assistant/AddAmbulence.php
+++ Emergency_Care_Assistant/AddAmbulence.php
@@ -7,52 +7,44 @@
 	</head>
 <html>
 
-
 		<section id="reg">
 			<div class="container">
 				<article id="main-colregister">
 					<h1 class="page-title">ADD DOCTOR</h1>
-			   <form class="amblist" action="AMB_ADD_DATABASE.php" method="POST">
-						
-						    <div>
-
+			   <form class="amblist" action="AMB_ADD_DATABASE.php" method="POST">		
+			    <div>
                                 <input type="text" placeholder="Ambulence Type" name="AMBULENCE_TYPE">
                             </div>
-							<div>
-
+			    <div>
                                 <input type="text" placeholder="Ambulence Number" name="LICENCE_NUMBER">
                             </div>
 
-							<div class="location2">
-							<div>
-								<select id="area" name="AREA_ID" onchange="readothervalues();">
+			    <div class="location2">
+				<div>
+				    <select id="area" name="AREA_ID" onchange="readothervalues();">
                                     <option value="-1" selected>Select Area</option>
-									<?php
-                                    try{
-                                    $con=new PDO("mysql:host=localhost;dbname=project",'root','');
-                                    	$stmt="select * from area";
-                                        
-                                        $pdostmt=$con->query($stmt);
-                                        $table=$pdostmt->fetchAll(PDO::FETCH_NUM);
-                                        
-                                        foreach($table as $row){
-                                            echo "<option value=$row[0]>$row[1]</option>";
-                                        }
-                                    }
-                                    catch(PDOException $ex){
-                                        
-                                    }
+					<?php
+					    try{
+					    $con=new PDO("mysql:host=localhost;dbname=project",'root','');
+						$stmt="select * from area";
+
+						$pdostmt=$con->query($stmt);
+						$table=$pdostmt->fetchAll(PDO::FETCH_NUM);
+
+						foreach($table as $row){
+						    echo "<option value=$row[0]>$row[1]</option>";
+						}
+					    }
+					    catch(PDOException $ex){
+
+					    }
                                     ?>
 
-								</select>
+				</select>
 
                             </div>
-                                <button class="register-button" type="submit" name="AMB">Confirm</button>
-								
-						
-				</form>
-				
-					
+                                <button class="register-button" type="submit" name="AMB">Confirm</button>		
+			</form>	
 				</article>
 			</div>
 		</section>
